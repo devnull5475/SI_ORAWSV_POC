1ddde63aba7b85c4eda13d781e86191b69e41db8
ASD <devnull5475@yahoo.com>
2014-12-16 08:57:15 -0500

    Add some spring-ws; lose all JDBC; explain clone.sh better

--- bin/clone.sh
+++ bin/clone.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo # On laptop
+echo # On computer that has git access to GitHub;
+echo # e.g., work laptop used to pull/push from GitHub at home.
 echo git clone --bare https://github.com/devnull5475/SI_ORAWSV_POC.git owsx.git
 
-echo # On desktop
+echo # On computer(s) that do not have git access to GitHub;
+echo # e.g., work desktop behind firewall.
 echo git clone /cygdrive/l/projects/bare/owsx.git owsx
