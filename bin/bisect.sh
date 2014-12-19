25bc5fc65417ed51be89de99f47fda30443a9d93
ASD <devnull5475@yahoo.com>
2014-12-19 14:26:42 -0500

    Allow bisect-related scripts to execute one JUnit test only.

--- bin/bisect.sh
+++ bin/bisect.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
-echo "NB: This won't work of $_BAD or $_GOOD change either this file or test.sh";
-_USAGE="$0 -b BAD -g GOOD\nNB: This won't work if \$_BAD or \$_GOOD change either this file or test.sh";
+# Use git bisect to find the commit that introduced a bug.
+# Info:
+#  http://java.dzone.com/articles/automated-bug-finding-git
+
+_USAGE="$0 -b BAD -g GOOD [-t one.failing.Test]" ;
 
 _BAD=HEAD;
 _GOOD=1.0;
 _VERBOSE=;
+_SINGLE_TEST=;
 
 OPTIND=1;
-while getopts "b:g:vh" opt; do
+while getopts "b:g:t:vh" opt; do
     case "${opt}" in
         h|\?)
             echo "${_USAGE}" && exit 0 ; ;;
         v)
-            _VERBOSE=1 ; ;;
+            _VERBOSE="-v" ; ;;
         b)
-            _BAD="${opt}" ; ;;
+            _BAD="${OPTARG}" ; ;;
         g)
-            _GOOD="${opt}" ; ;;
+            _GOOD="${OPTARG}" ; ;;
+        t)
+            _SINGLE_TEST="${OPTARG}" ; ;;
     esac
 done
 
-[[ -z "${_BAD}" ]] && echo "${_USAGE}" && exit 1 ;
-[[ -z "${_GOOD}" ]] && echo "${_USAGE}" && exit 1 ;
+[[ -z "${_BAD}" ]] && echo "Missing BAD.\n${_USAGE}" && exit 1 ;
+[[ -z "${_GOOD}" ]] && echo "Missing GOOD.\n${_USAGE}" && exit 1 ;
 
-[[ -n "${_VERBOSE}" ]] && echo "git bisect \$_BAD=${_BAD} \$_GOOD=${_GOOD} ./bin/test.sh"
+[[ -n "${_VERBOSE}" ]] && echo "git bisect \$_BAD=${_BAD} \$_GOOD=${_GOOD} ./bin/test.sh -t\"${_SINGLE_TEST}\" ${_VERBOSE}"
 
-git bisect reset
-git bisect start
-git bisect bad "${_BAD}"
-git bisect good "${_GOOD}"
-git bisect run ./bin/test.sh
+#git bisect reset
+#git bisect start
+#git bisect bad "${_BAD}"
+#git bisect good "${_GOOD}"
+#git bisect run ./bin/test.sh -t "${_SINGLE_TEST} ${_VERBOSE}"
+. ./bin/test.sh -t "${_SINGLE_TEST}" "${_VERBOSE}"
--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-mvn clean test
+_USAGE="$0 [-t org.our_firm.our_app.TestNewBug]";
+_SINGLE_TEST=;
+
+OPTIND=1;
+while getopts "t:vh" opt; do
+    case "${opt}" in
+        h|\?)
+            echo "${_USAGE}" && exit 0 ; ;;
+        v)
+            _VERBOSE=1 ; ;;
+        t)
+            _SINGLE_TEST="${OPTARG}" ; ;;
+    esac
+done
+
+[[ -n "${_VERBOSE}" ]] && echo "\$_SINGLE_TEST=${_SINGLE_TEST}" ;
+
+if [[ -z "${_SINGLE_TEST}" ]] ; then
+ mvn clean test ;
+else
+ mvn clean -Dtest=${_SINGLE_TEST} test ;
+fi
+
 rc=$?
 if [[ $rc != 0 ]] ; then
     return 1 ;
